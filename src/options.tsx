3f0ccfe2b814bbc413d6345871c45e79c7d0cc79
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2021-12-28 01:39:08 +0200

    fixed settings

--- src/options.tsx
+++ src/options.tsx
@@ -3,26 +3,29 @@ import ReactDOM from "react-dom";
 import { allModules } from "./content_script"
 import { config } from "./Storage/api"
 
+
+const divStyle = {
+    border: "1px solid black",
+};
+
+
 const Options = () => {
     const [options, setOptions] = useState(Array<JSX.Element>())
 
     useEffect(() => {
-        allModules.forEach(module => {
-            config.onload(() => {
-                const ops = module.options(config)
-                if (ops != undefined) {
-                    setOptions([...options, ops])
-                }
-            })
+        config.onload(() => {
+            let ops = allModules.map(m => m.options(config)).filter(o => o != undefined) as Array<JSX.Element>
+            setOptions(ops)
         })
     }, []);
 
     return (
         <>
-            {options.map(option => (
-                <div>{option}</div>
+            {options.map((option) => (
+                <div style={divStyle}>
+                    {option}
+                </div>
             ))}
-
         </>
     );
 };
