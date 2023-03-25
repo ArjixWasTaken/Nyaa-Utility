63d65569a9c3e8b950b94d4a8f094b7c5e69e943
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-03-25 23:42:15 +0200

    uhhh

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,31 @@
 import browser from "webextension-polyfill";
+import { allModules } from "./Modules";
+import { config } from "./storage";
+import Logger from "./utils/Logger";
 
-console.log("Hello from the background!");
+const logger = new Logger("background");
 
-browser.runtime.onInstalled.addListener((details) => {
-    console.log("Extension installed:", details);
+config.liveSync()
+
+// We can't modify the tabs before the browser has loaded,
+// and for some reason the extension loads before that...
+browser.runtime.onInstalled.addListener(() => {
+    setTimeout(() => {
+        if (import.meta && import.meta.env && import.meta.env.DEV) {
+            browser.tabs.query({}).then(tabs => {
+                if (!tabs.length) return;
+                const tab = tabs[0]
+
+                console.log({ tab, url: tab.url })
+
+                if (tab.url && !tab.url.includes("nyaa.si")) {
+                    logger.debug("Redirecting to nyaa.si")
+                    browser.tabs.update(tab.id, { url: "https://nyaa.si" });
+                }
+            })
+        }
+    }, 1000)
 });
-
 
 interface Clock {
     moduleId: string
@@ -13,30 +33,38 @@ interface Clock {
 }
 
 const Clock = {
-    clocks: [{ moduleId: "test", countdown: 10 }],
-    tick() {
-        for (const clock of this.clocks) {
-            clock.countdown -= 1;
+    clocks: allModules.map(mod => {
+        return mod.backgroundTaskInterval ? { moduleId: mod.id, countdown: mod.backgroundTaskInterval } : undefined
+    }).filter(Boolean) as Array<Clock>,
 
+    tick() {
+        for (const clock of Clock.clocks) {
+            clock.countdown -= 1
+            if (clock.countdown <= 0) {
+                const cb = allModules.find(i => i.id === clock.moduleId);
+                if (cb && cb.backgroundTask) {
+                    try {
+                        cb.backgroundTask(config)
+                        logger.info(`Background task for \`${cb.id}\` was ran.`)
+                    } catch (e) {
+                        logger.error("Failed to run background task for", cb.id, e)
+                    } finally {
+                        clock.countdown = cb.backgroundTaskInterval!
+                    }
+                }
+            }
         }
     }
 }
 
-// chrome.alarms.create(
-//     name?: string,
-//     alarmInfo: AlarmCreateInfo,
-//     callback?: function,
-//   )
 
+// Due to the a limitation in the alarms API, the first alarm will fire two minutes after it's set.
+// But from the 2nd fire and on, it will fire every minute.
+// We don't need to run a background task at an internal less than 5 minutes, so it is ok.
+// Although it is annoying for the developer, since you have to wait 2 minutes for the alarm to fire.
 browser.alarms.create("clock", { delayInMinutes: 1, periodInMinutes: 1 })
 browser.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "clock") {
         Clock.tick()
     }
 })
-
-if (import.meta && import.meta.env && import.meta.env.DEV) {
-    browser.tabs.getCurrent().then((tab) => {
-        browser.tabs.update(tab.id, { url: "https://nyaa.si" });
-    });
-}
--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,28 @@
-import browser from "webextension-polyfill";
+import Logger from "./utils/Logger";
+import { allModules } from "./Modules";
+import { config } from "./storage";
 
-console.log("Hello from content");
+config.liveSync();
+const logger = new Logger("Page");
+
+// TODO: Actually implement a menu to enable/disable modules.
+const enabledModules: String[] = allModules.map(mod => mod.id)
+
+allModules.forEach(module => {
+    try {
+        if (enabledModules.includes(module.id) && module.shouldRun.test(window.location.href)) {
+            if (module.injectWithConfig) {
+                config.onload(() => {
+                    module.inject(config)
+                    logger.info(`Loaded '${module.id}'`)
+                })
+                return;
+            }
+
+            module.inject(config)
+            logger.info(`Loaded '${module.id}'`)
+        }
+    } catch (error) {
+        logger.error(`Error loading module '${module.id}'`, error)
+    }
+})
